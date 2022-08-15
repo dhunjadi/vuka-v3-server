624b8ea906db4e8921949d9724f7f529f61734df
Dario Hunjadi <dario.hunjadi@hotmail.com>
2022-08-15 13:39:57 +0200

    changed port to 4000, connecteded to mongodb with URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { mongoURI } from './config/keys';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot(mongoURI)],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
---
+++ src/config/keys.ts
@@ -0,0 +1,3 @@
+/* eslint-disable prettier/prettier */
+export const mongoURI =
+  'mongodb+srv://testUser:Password12345@cluster0.kyjyns7.mongodb.net/?retryWrites=true&w=majority';
